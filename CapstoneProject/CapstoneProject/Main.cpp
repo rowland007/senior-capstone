2dac7937cc9090135dd49b2f30ad952adf96129f
Randall Rowland <rowland007@gmail.com>
2016-07-05 18:57:58 -0400

    Created empty main.cpp file.  Loaded it with comment header for use in other files as a template.

---
+++ CapstoneProject/CapstoneProject/Main.cpp
@@ -0,0 +1,23 @@
+/************************************************************************
+Program: CapstoneProject
+Author: Randall D. Rowland Jr.
+Class:
+Instructor: N/A
+Date: 
+Description:
+Input:
+Output:
+Compilation instructions:
+Usage:
+Known bugs/missing features:
+
+This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
+International Public License. To view a copy of this license, visit
+https://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to Creative
+Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+
+Modifications:
+Date                Comment
+----    ------------------------------------------------
+
+************************************************************************/
