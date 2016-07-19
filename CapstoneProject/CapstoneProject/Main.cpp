88b2969dc4001f020b93a1748c789a561abe726b
Randall Rowland <rowland007@gmail.com>
2016-07-18 20:52:15 -0400

    Added a generic icon for the binary and window.  Added the resource files to enable the icon to load into the program.  Wrote the main part of the game that utilizes the game engine to create a window and register it with the OS.  Main loop right now has a test line in it to load the icon and text to show that it is functioning.

--- CapstoneProject/CapstoneProject/Main.cpp
+++ CapstoneProject/CapstoneProject/Main.cpp
@@ -2,7 +2,7 @@
 Program: CapstoneProject
 Author: Randall D. Rowland Jr.
 Class:
-Instructor: N/A
+Instructor: Dan Randall
 Date: 
 Description:
 Input:
@@ -21,3 +21,79 @@ Date                Comment
 ----    ------------------------------------------------
 
 ************************************************************************/
+#include <Windows.h>
+#include "GameEngine.h"
+#include "Resource.h"
+
+GameEngine* g_pGame;
+
+BOOL GameInitialize(HINSTANCE hInstance)
+{
+	//Create the game engine
+	g_pGame = new GameEngine(hInstance, TEXT("Dungeon Warrior"), TEXT("Dungeon Warrior"), IDI_ICON1, IDI_ICON1);
+
+	if (g_pGame == NULL)
+		return FALSE;
+
+	g_pGame->SetFrameRate(15);
+	
+	return TRUE;
+}
+
+void GameStart(HWND hWindow)
+{
+	//Seed random number generator
+	srand(GetTickCount());
+}
+
+void GameEnd()
+{
+	//clean the system
+	delete g_pGame;
+}
+
+void GameActivate(HWND hWindow)
+{
+	HDC		hDC;
+	RECT	rect;
+
+	//Draw activation text on screen
+	GetClientRect(hWindow, &rect);
+	hDC = GetDC(hWindow);
+
+	DrawText(hDC, TEXT("TEST WINDOW - ACTIVE"), -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+	ReleaseDC(hWindow, hDC);
+}
+
+void GameDeactivate(HWND hWindow)
+{
+	HDC		hDC;
+	RECT	rect;
+
+	//Draw deactivation text on screen
+	GetClientRect(hWindow, &rect);
+	hDC = GetDC(hWindow);
+
+	DrawText(hDC, TEXT("TEST WINDOW - NOT ACTIVE"), -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+	ReleaseDC(hWindow, hDC);
+}
+
+void GamePaint(HDC hDC)
+{
+	//empty
+}
+
+void GameCycle()
+{
+	HDC		hDC;
+	HWND	hWindow = g_pGame->GetWindow();
+
+	//Draw the screen
+	hDC = GetDC(hWindow);
+
+	//This will draw your icon on the screen randomly.  This is a test module and can be removed at a later date
+	DrawIcon(hDC, rand() % g_pGame->GetWidth(), rand() % g_pGame->GetHeight(), (HICON)(WORD)GetClassLong(hWindow, GCL_HICON));
+
+	//clean up
+	ReleaseDC(hWindow, hDC);
+}
