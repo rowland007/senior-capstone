a2a51ddd9312fca047623011d2c6dbd416812611
Randy Rowland <rowland007@gmail.com>
2016-08-07 15:21:54 -0600

    Updated the Sprite class. Still needs its inline functions written.

--- CapstoneProject/CapstoneProject/Sprite.cpp
+++ CapstoneProject/CapstoneProject/Sprite.cpp
@@ -16,36 +16,244 @@ Known bugs/missing features:
 Modifications:
 Date                Comment
 ----    ------------------------------------------------
-************************************************************************
+************************************************************************/
 #include "Sprite.h"
-#include "GameEngine.h"
 
-Sprite::Sprite(Bitmap *pBitmap, int iGameSizeWidth, int iGameSizeHeight)
+//-----------------------------------------------------------------
+// Sprite Constructor(s)/Destructor
+//-----------------------------------------------------------------
+Sprite::Sprite(Bitmap* pBitmap)
 {
-	RECT rect;
-	m_pBitmap = pBitmap;
-	m_iNumFrames = 1;
-	m_iCurFrame = m_iFrameDelay = m_iFrameTrigger = 0;
-	SetRect(&m_rcPosition, 0, 0, pBitmap->GetWidth(), pBitmap->GetHeight());
-	CalcCollisionRect();
-	m_ptVeloctiy.x = m_ptVeloctiy.y = 0;
-	m_iZorder = 0;
-	SetRect(&m_rcBounds, 0, 0, iGameSizeWidth, iGameSizeHeight);
-	m_baBoundsAction = BA_STOP;
-	m_bHidden = FALSE;
-	m_bDying = FALSE;
-	m_bOneCycle = FALSE;
+  // Initialize the member variables
+  m_pBitmap = pBitmap;
+  m_iNumFrames = 1;
+  m_iCurFrame = m_iFrameDelay = m_iFrameTrigger = 0;
+  SetRect(&m_rcPosition, 0, 0, pBitmap->GetWidth(), pBitmap->GetHeight());
+  CalcCollisionRect();
+  m_ptVelocity.x = m_ptVelocity.y = 0;
+  m_iZOrder = 0;
+  SetRect(&m_rcBounds, 0, 0, 640, 480);
+  m_baBoundsAction = BA_STOP;
+  m_bHidden = false;
+  m_bDying = false;
+  m_bOneCycle = false;
 }
 
-Sprite::Sprite(Bitmap *pBitmap, RECT &rcBounds, BOUNDSACTION baBoundsAction)
+Sprite::Sprite(Bitmap* pBitmap, RECT& rcBounds, BOUNDSACTION baBoundsAction)
 {
-	m_pBitmap = pBitmap;
-	m_iNumFrames = 1;
-	m_iCurFrame = m_iFrameDelay = m_iFrameTrigger = 0;
-	SetRect(&m_rcPosition, (rcBounds.right / 2), (rcBounds.bottom / 2), (rcBounds.right / 2) + pBitmap->GetWidth(), (rcBounds.bottom / 2) + pBitmap->GetHeight());
-	//TODO: Finish Init vars
+  // Calculate a random position
+  int iXPos = rand() % (rcBounds.right - rcBounds.left);
+  int iYPos = rand() % (rcBounds.bottom - rcBounds.top);
+
+  // Initialize the member variables
+  m_pBitmap = pBitmap;
+  m_iNumFrames = 1;
+  m_iCurFrame = m_iFrameDelay = m_iFrameTrigger = 0;
+  SetRect(&m_rcPosition, iXPos, iYPos, iXPos + pBitmap->GetWidth(), iYPos + pBitmap->GetHeight());
+  CalcCollisionRect();
+  m_ptVelocity.x = m_ptVelocity.y = 0;
+  m_iZOrder = 0;
+  CopyRect(&m_rcBounds, &rcBounds);
+  m_baBoundsAction = baBoundsAction;
+  m_bHidden = false;
+  m_bDying = false;
+  m_bOneCycle = false;
+}
+
+Sprite::Sprite(Bitmap* pBitmap, POINT ptPosition, POINT ptVelocity, int iZOrder, RECT& rcBounds, BOUNDSACTION baBoundsAction)
+{
+  // Initialize the member variables
+  m_pBitmap = pBitmap;
+  m_iNumFrames = 1;
+  m_iCurFrame = m_iFrameDelay = m_iFrameTrigger = 0;
+  SetRect(&m_rcPosition, ptPosition.x, ptPosition.y, ptPosition.x + pBitmap->GetWidth(), ptPosition.y + pBitmap->GetHeight());
+  CalcCollisionRect();
+  m_ptVelocity = ptVelocity;
+  m_iZOrder = iZOrder;
+  CopyRect(&m_rcBounds, &rcBounds);
+  m_baBoundsAction = baBoundsAction;
+  m_bHidden = false;
+  m_bDying = false;
+  m_bOneCycle = false;
 }
 
 Sprite::~Sprite()
 {
-}*/
+}
+
+//-----------------------------------------------------------------
+// Sprite General Methods
+//-----------------------------------------------------------------
+SPRITEACTION Sprite::Update()
+{
+  // See if the sprite needs to be killed
+  if (m_bDying)
+    return SA_KILL;
+
+  // Update the frame
+  UpdateFrame();
+
+  // Update the position
+  POINT ptNewPosition, ptSpriteSize, ptBoundsSize;
+  ptNewPosition.x = m_rcPosition.left + m_ptVelocity.x;
+  ptNewPosition.y = m_rcPosition.top + m_ptVelocity.y;
+  ptSpriteSize.x = m_rcPosition.right - m_rcPosition.left;
+  ptSpriteSize.y = m_rcPosition.bottom - m_rcPosition.top;
+  ptBoundsSize.x = m_rcBounds.right - m_rcBounds.left;
+  ptBoundsSize.y = m_rcBounds.bottom - m_rcBounds.top;
+
+  // Check the bounds
+  // Wrap?
+  if (m_baBoundsAction == BA_WRAP)
+  {
+    if ((ptNewPosition.x + ptSpriteSize.x) < m_rcBounds.left)
+      ptNewPosition.x = m_rcBounds.right;
+    else if (ptNewPosition.x > m_rcBounds.right)
+      ptNewPosition.x = m_rcBounds.left - ptSpriteSize.x;
+    if ((ptNewPosition.y + ptSpriteSize.y) < m_rcBounds.top)
+      ptNewPosition.y = m_rcBounds.bottom;
+    else if (ptNewPosition.y > m_rcBounds.bottom)
+      ptNewPosition.y = m_rcBounds.top - ptSpriteSize.y;
+  }
+  // Bounce?
+  else if (m_baBoundsAction == BA_BOUNCE)
+  {
+    bool bBounce = false;
+    POINT ptNewVelocity = m_ptVelocity;
+    if (ptNewPosition.x < m_rcBounds.left)
+    {
+      bBounce = true;
+      ptNewPosition.x = m_rcBounds.left;
+      ptNewVelocity.x = -ptNewVelocity.x;
+    }
+    else if ((ptNewPosition.x + ptSpriteSize.x) > m_rcBounds.right)
+    {
+      bBounce = true;
+      ptNewPosition.x = m_rcBounds.right - ptSpriteSize.x;
+      ptNewVelocity.x = -ptNewVelocity.x;
+    }
+    if (ptNewPosition.y < m_rcBounds.top)
+    {
+      bBounce = true;
+      ptNewPosition.y = m_rcBounds.top;
+      ptNewVelocity.y = -ptNewVelocity.y;
+    }
+    else if ((ptNewPosition.y + ptSpriteSize.y) > m_rcBounds.bottom)
+    {
+      bBounce = true;
+      ptNewPosition.y = m_rcBounds.bottom - ptSpriteSize.y;
+      ptNewVelocity.y = -ptNewVelocity.y;
+    }
+    if (bBounce)
+      SetVelocity(ptNewVelocity);
+  }
+  // Die?
+  else if (m_baBoundsAction == BA_DIE)
+  {
+    if ((ptNewPosition.x + ptSpriteSize.x) < m_rcBounds.left ||
+      ptNewPosition.x > m_rcBounds.right ||
+      (ptNewPosition.y + ptSpriteSize.y) < m_rcBounds.top ||
+      ptNewPosition.y > m_rcBounds.bottom)
+      return SA_KILL;
+  }
+  // Stop (default)
+  else
+  {
+    if (ptNewPosition.x  < m_rcBounds.left ||
+      ptNewPosition.x > (m_rcBounds.right - ptSpriteSize.x))
+    {
+      ptNewPosition.x = max(m_rcBounds.left, min(ptNewPosition.x,
+        m_rcBounds.right - ptSpriteSize.x));
+      SetVelocity(0, 0);
+    }
+    if (ptNewPosition.y  < m_rcBounds.top ||
+      ptNewPosition.y > (m_rcBounds.bottom - ptSpriteSize.y))
+    {
+      ptNewPosition.y = max(m_rcBounds.top, min(ptNewPosition.y,
+        m_rcBounds.bottom - ptSpriteSize.y));
+      SetVelocity(0, 0);
+    }
+  }
+  SetPosition(ptNewPosition);
+
+  return SA_NONE;
+}
+
+void Sprite::Draw(HDC hDC)
+{
+  // Draw the sprite if it isn't hidden
+  if (m_pBitmap != NULL && !m_bHidden)
+  {
+    // Draw the appropriate frame, if necessary
+    if (m_iNumFrames == 1)
+      m_pBitmap->Draw(hDC, m_rcPosition.left, m_rcPosition.top, true);
+    else
+      m_pBitmap->DrawPart(hDC, m_rcPosition.left, m_rcPosition.top,
+        0, m_iCurFrame * GetHeight(), GetWidth(), GetHeight(), true);
+  }
+}
+
+Bitmap Sprite::GetBitmap() const
+{
+	return m_pBitmap;
+}
+
+void Sprite::SetFrameDelay(int iFrameDelay)
+{
+	m_iFrameDelay = iFrameDelay;
+}
+
+RECT Sprite::GetPosition() const
+{
+	return m_rcPosition;
+}
+
+RECT Sprite::GetCollision() const
+{
+	return m_rcCollision;
+}
+
+POINT Sprite::GetVelocity() const
+{
+	return m_ptVeloctiy;
+}
+
+bool Sprite::GetZorder() const
+{
+	return m_iZorder;
+}
+
+void Sprite::SetZorder(int iZOrder)
+{
+	m_iZorder = iZOrder;
+}
+
+void Sprite::SetBounds(RECT& rcBounds)
+{
+	CopyRect(&m_rcBounds, &rcBounds);
+}
+
+void Sprite::SetBoundsAction(BOUNDSACTION ba)
+{
+	m_baBoundsAction = ba;
+}
+
+bool Sprite::IsHidden() const
+{
+	return m_bHidden;
+}
+
+void Sprite::SetHidden(bool bHidden)
+{
+	m_bHidden = bHidden;
+}
+
+int Sprite::GetWidth() const
+{
+	return m_pBitmap->GetWidth();
+}
+
+int Sprite::GetHeight() const
+{
+	return m_pBitmap->GetHeight() / m_iNumFrames;
+}
--- CapstoneProject/CapstoneProject/Sprite.h
+++ CapstoneProject/CapstoneProject/Sprite.h
@@ -17,7 +17,7 @@ Modifications:
 Date                Comment
 ----    ------------------------------------------------
 22Jul16	Added const to getter functions. Removed implementation.
-************************************************************************
+************************************************************************/!
 #pragma once
 #include <Windows.h>
 #include "Bitmap.h"
@@ -37,18 +37,18 @@ const BOUNDSACTION	BA_STOP		= 0,
 class Sprite
 {
 public:
-	Sprite(Bitmap *pBitmap, int iGameSizeWidth, int iGameSizeHeight);
+	Sprite(Bitmap *pBitmap);
 	Sprite(Bitmap *pBitmap, RECT &rcBounds, BOUNDSACTION baBoundsAction = BA_STOP);
 	Sprite(Bitmap *pBitmap, POINT ptPosition, POINT ptVelocity, int iZorder, RECT &rcBounds, BOUNDSACTION = BA_STOP);
 	virtual ~Sprite();
 	virtual SPRITEACTION Update();
 	Sprite *AddSprite;
 	void Draw(HDC hDC);
-	BOOL IsPointInside(int x, int y);
-	BOOL TestCollision(Sprite *pTestSprite);
+	bool IsPointInside(int x, int y);
+	bool TestCollision(Sprite *pTestSprite);
 	void Kill();
 	Bitmap *GetBitmap() const;
-	void SetNumFrames(int iNumFrames, BOOL bOneCycle = FALSE);
+	void SetNumFrames(int iNumFrames, bool bOneCycle = false);
 	void SetFrameDelay(int iFrameDelay);
 	RECT &GetPosition() const;
 	void SetPosition(int x, int y);
@@ -59,12 +59,12 @@ public:
 	POINT GetVelocity() const;
 	void SetVelocity(int x, int y);
 	void SetVelocity(POINT ptVelocity);
-	BOOL GetZorder() const;
+	bool GetZorder() const;
 	void SetZorder(int iZorder);
 	void SetBounds(RECT &rcBounds);
 	void SetBoundsAction(BOUNDSACTION ba);
-	BOOL IsHidden() const;
-	void SetHidden(BOOL bHidden);
+	bool IsHidden() const;
+	void SetHidden(bool bHidden);
 	int GetWidth() const;
 	int GetHeight() const;
 protected:
@@ -79,10 +79,10 @@ protected:
 	RECT			m_rcCollision;
 	RECT			m_rcBounds;
 	BOUNDSACTION	m_baBoundsAction;
-	BOOL			m_bHidden;
-	BOOL			m_bDying;
-	BOOL			m_bOneCycle;
+	bool			m_bHidden;
+	bool			m_bDying;
+	bool			m_bOneCycle;
 
 	void UpdateFrame();
 	virtual void CalcCollisionRect();
-};*/
+};
