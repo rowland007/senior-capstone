c14cb725c0a098dbd758a7553a25fc3247a8eb2a
Randy D Rowland Jr <rowland007@gmail.com>
2016-08-18 18:31:13 -0400

    Added offscreen device for double buffering and sprite collision detection

--- CapstoneProject/CapstoneProject/Main.cpp
+++ CapstoneProject/CapstoneProject/Main.cpp
@@ -25,6 +25,7 @@ Date                Comment
 20Jul16 Added keyboard support with HandleKeys function.
 22Jul16	Modified HandleKeys to a switch/case.
 3Aug16	Set screen size in GameEngine constructor parameters
+18Aug16	Added offscreen device for double buffering and sprite collision detection
 ************************************************************************/
 #include <Windows.h>
 #include "GameEngine.h"
@@ -58,14 +59,22 @@ bool GameInitialize(HINSTANCE hInstance)
 
 void GameStart(HWND hWindow)
 {
-	HDC hDC = GetDC(hWindow);
+	  // Create the offscreen device context and bitmap
+  	g_hOffscreenDC = CreateCompatibleDC(GetDC(hWindow));
+  	g_hOffscreenBitmap = CreateCompatibleBitmap(GetDC(hWindow), g_pGame->GetWidth(), g_pGame->GetHeight());
+  	SelectObject(g_hOffscreenDC, g_hOffscreenBitmap);
+
 	isLoading = true;
-	g_pLoadScreen = new Bitmap(hDC, IDB_BITMAP1, g_hInstance);
+	g_pLoadScreen = new Bitmap(GetDC(hWindow), IDB_BITMAP1, g_hInstance);
 	g_pLoadScreenText = new Bitmap(hDC, IDB_BITMAP2, g_hInstance);
 }
 
 void GameEnd()
 {
+  	// Cleanup the offscreen device context and bitmap
+  	DeleteObject(g_hOffscreenBitmap);
+  	DeleteDC(g_hOffscreenDC); 
+
 	//Deallocation of memory
 	delete g_pLoadScreenText;
 	g_pLoadScreenText = NULL;
@@ -113,11 +122,32 @@ void GamePaint(HDC hDC)
 
 void GameCycle()
 {
-	HDC		hDC;
-	HWND	hWindow = g_pGame->GetWindow();
-	hDC = GetDC(hWindow);
+  	if (!g_bGameOver)
+  	{
+ 
+    	// Update the sprites
+    	g_pGame->UpdateSprites();
 
-	ReleaseDC(hWindow, hDC);
+    	// Obtain a device context for repainting the game
+    	HWND  hWindow = g_pGame->GetWindow();
+    	HDC   hDC = GetDC(hWindow);
+
+    	// Paint the game to the offscreen device context
+    	GamePaint(g_hOffscreenDC);
+
+    	// Blit the offscreen bitmap to the game screen
+    	BitBlt(hDC, 0, 0, g_pGame->GetWidth(), g_pGame->GetHeight(), g_hOffscreenDC, 0, 0, SRCCOPY);
+
+    	// Cleanup
+    	ReleaseDC(hWindow, hDC);
+  	}
+  	else
+    	if (--g_iGameOverDelay == 0)
+    	{
+     	//Switch back to the LoadingScreen
+      	isLoading = true;
+      	NewGame();
+    	}
 }
 
 void HandleKeys(WPARAM wParam)
@@ -178,4 +208,116 @@ void HandleKeys(WPARAM wParam)
 		break;
 	}
 	ReleaseDC(hWindow, hDC);
+}
+bool SpriteCollision(Sprite* pSpriteHitter, Sprite* pSpriteHittee)
+{
+  	// See if a player missile and an alien have collided
+  	Bitmap* pHitter = pSpriteHitter->GetBitmap();
+  	Bitmap* pHittee = pSpriteHittee->GetBitmap();
+  	if ((pHitter == g_pMissileBitmap && (pHittee == g_pBlobboBitmap || pHittee == g_pJellyBitmap || pHittee == g_pTimmyBitmap)) || 
+	  			(pHittee == g_pMissileBitmap && (pHitter == g_pBlobboBitmap || pHitter == g_pJellyBitmap || pHitter == g_pTimmyBitmap)))
+  	{
+    	// Play the small explosion sound
+    	PlaySound((LPCSTR)IDW_LGEXPLODE, g_hInstance, SND_ASYNC | SND_RESOURCE);
+
+	    // Kill both sprites
+	    pSpriteHitter->Kill();
+	    pSpriteHittee->Kill();
+
+    	// Create a large explosion sprite at the alien's position
+    	RECT rcBounds = { 0, 0, 600, 450 };
+    	RECT rcPos;
+    	if (pHitter == g_pMissileBitmap)
+      		rcPos = pSpriteHittee->GetPosition();
+    	else
+      		rcPos = pSpriteHitter->GetPosition();
+    
+		Sprite* pSprite = new Sprite(g_pLgExplosionBitmap, rcBounds);
+    	pSprite->SetNumFrames(8, TRUE);
+    	pSprite->SetPosition(rcPos.left, rcPos.top);
+    	g_pGame->AddSprite(pSprite);
+ 	}
+
+	// See if an alien missile has collided with the car
+  	if ((pHitter == g_pCarBitmap && (pHittee == g_pBMissileBitmap || pHittee == g_pJMissileBitmap || pHittee == g_pTMissileBitmap)) ||
+    		(pHittee == g_pCarBitmap && (pHitter == g_pBMissileBitmap || pHitter == g_pJMissileBitmap || pHitter == g_pTMissileBitmap)))
+  	{
+    	// Play the large explosion sound
+    	PlaySound((LPCSTR)IDW_LGEXPLODE, g_hInstance, SND_ASYNC | SND_RESOURCE);
+
+    	// Kill the missile sprite
+    	if (pHitter == g_pCarBitmap)
+      		pSpriteHittee->Kill();
+    	else
+      		pSpriteHitter->Kill();
+
+    	// Create a large explosion sprite at the car's position
+    	RECT rcBounds = { 0, 0, 600, 480 };
+    	RECT rcPos;
+    	if (pHitter == g_pCarBitmap)
+      		rcPos = pSpriteHitter->GetPosition();
+    	else
+      		rcPos = pSpriteHittee->GetPosition();
+    	
+		Sprite* pSprite = new Sprite(g_pLgExplosionBitmap, rcBounds);
+    	pSprite->SetNumFrames(8, TRUE);
+    	pSprite->SetPosition(rcPos.left, rcPos.top);
+    	g_pGame->AddSprite(pSprite);
+
+    	// Move the car back to the start
+    	g_pCarSprite->SetPosition(300, 405);
+
+    	// See if the game is over
+    	if (--g_iNumLives == 0)
+    	{
+      		// Play the game over sound
+      		PlaySound((LPCSTR)IDW_GAMEOVER, g_hInstance, SND_ASYNC | SND_RESOURCE);
+      		g_bGameOver = TRUE;
+      		g_iGameOverDelay = 150;
+
+    	}
+  	}
+
+  	return false;
+}
+
+void SpriteDying(Sprite* pSpriteDying)
+{
+  	// See if an alien missile sprite is dying
+  	if (pSpriteDying->GetBitmap() == g_pBMissileBitmap || pSpriteDying->GetBitmap() == g_pJMissileBitmap || pSpriteDying->GetBitmap() == g_pTMissileBitmap)
+  	{
+    	// Play the small explosion sound
+      	PlaySound((LPCSTR)IDW_SMEXPLODE, g_hInstance, SND_ASYNC | SND_RESOURCE | SND_NOSTOP);
+
+    	// Create a small explosion sprite at the missile's position
+    	RECT rcBounds = { 0, 0, 600, 450 };
+    	RECT rcPos = pSpriteDying->GetPosition();
+    	Sprite* pSprite = new Sprite(g_pSmExplosionBitmap, rcBounds);
+    	pSprite->SetNumFrames(8, TRUE);
+    	pSprite->SetPosition(rcPos.left, rcPos.top);
+    	g_pGame->AddSprite(pSprite);
+  	}
+}
+
+//-----------------------------------------------------------------
+// Functions
+//-----------------------------------------------------------------
+void NewGame()
+{
+  	// Clear the sprites
+  	g_pGame->CleanupSprites();
+
+  	// Initialize the game variables
+  	g_bGameOver = false;
+
+  	if (isLoading)
+  	{
+    	// Add 
+  	}
+  	else
+  	{
+    	// Create 
+    	// Play the background music
+    	//g_pGame->PlayMIDISong(TEXT(""));
+  	}
 }
