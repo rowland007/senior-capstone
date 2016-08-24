cf0b0113bc8fba4b801e602e28c5e19add6596c4
Randall Rowland <rowland007@gmail.com>
2016-08-24 00:31:57 -0400

    Added Hero class.  Member methods and fields definitions complete but still need to complete implementation.

---
+++ CapstoneProject/CapstoneProject/Hero.cpp
@@ -0,0 +1,54 @@
+#include "Hero.h"
+
+
+
+Hero::Hero()
+{
+}
+
+
+Hero::~Hero()
+{
+}
+
+void Hero::MoveLeft()
+{
+}
+
+void Hero::MoveRight()
+{
+}
+
+void Hero::MoveUp()
+{
+}
+
+void Hero::MoveDown()
+{
+}
+
+void Hero::SetWeapon(WEAPONTYPE)
+{
+}
+
+WEAPONTYPE Hero::GetWeapon() const
+{
+	return WEAPONTYPE();
+}
+
+void Hero::SetHealth(int)
+{
+}
+
+int Hero::GetHealth() const
+{
+	return 0;
+}
+
+void Hero::UseWeapon()
+{
+}
+
+void Hero::IncreaseHealthMeter()
+{
+}
---
+++ CapstoneProject/CapstoneProject/Hero.h
@@ -0,0 +1,49 @@
+/************************************************************************
+Program:		CapstoneProject
+Author:			Randy Rowland
+Class:			Hero
+Instructor:		Dan Randall
+Date:			23 August 2016
+Description:	
+Input:			
+Output:
+
+Compilation instructions:
+Usage:
+Known bugs/missing features:
+
+Modifications:
+Date                Comment
+----    ------------------------------------------------
+
+************************************************************************/
+#pragma once
+#include "Sprite.h"
+
+typedef WORD		WEAPONTYPE;
+const WEAPONTYPE	WT_SWORD = 0,
+					WT_BOOMERANG = 1,
+					WT_BOW = 2;
+
+class Hero : public Sprite
+{
+public:
+	Hero();
+	~Hero();
+	void MoveLeft();
+	void MoveRight();
+	void MoveUp();
+	void MoveDown();
+	void SetWeapon(WEAPONTYPE);
+	WEAPONTYPE GetWeapon() const;
+	void SetHealth(int);
+	int GetHealth() const;
+	void UseWeapon();
+	void IncreaseHealthMeter();
+
+private:
+	int health;
+	int healthMeter;
+	WEAPONTYPE weapon;
+};
+
