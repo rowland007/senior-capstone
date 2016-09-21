c8ce8fe8293f2b9ab5b9104708f5dbe7c2d858ae
Randall Rowland <rowland007@gmail.com>
2016-09-21 05:45:11 -0400

    Collision detection implemented and minor changes
    Collision detection is written but needs some tweaks on how some of the pointers are accessed.

--- CapstoneProject/CapstoneProject/Main.cpp
+++ CapstoneProject/CapstoneProject/Main.cpp
@@ -38,8 +38,8 @@ Date                Comment
 #include "GameEngine.h"
 #include "Resource.h"
 #include "Sprite.h"
-#include "Construct.h"
-#include "Dungeon.h"
+#include "Hero.h"
+#include "EnemyBoss.h"
 
 //Required global variables
 bool				isTest = false;
@@ -66,7 +66,7 @@ static RECT			g_rcRight;
 Bitmap* g_pLoadScreen;
 Bitmap* g_pLoadScreenText;
 Bitmap* g_pLoadScreenSelectorBitmap;
-Bitmap* g_pDialogBox; //used in Construct and Load Screen
+Bitmap* g_pDialogBox; //used in load screen, construct, and dungeon.
 //Construct
 Bitmap* g_pConstructFloor;
 Bitmap* g_pConstructUpperWallBitmap;
@@ -198,9 +198,9 @@ void GameStart(HWND hWindow)
   	SelectObject(g_hOffscreenDC, g_hOffscreenBitmap);
 
 	//This sets the "level" to the loading screen
-	isLoading = false;
+	isLoading = true;
 	isConstruct = false;
-	isDungeon = true;
+	isDungeon = false;
 
 	//Create 3 little rectangles to be used for the sprite that is the menu item selector
 	//Will be used to determine which rectangle the sprite is within  
@@ -427,7 +427,6 @@ void GameStart(HWND hWindow)
 		g_pHero = new Hero(g_pHeroBitmap, g_rcEnemeyBounds, BA_STOP);
 		g_pHero->SetPosition(g_pGame->GetWidth() / 2, g_pGame->GetHeight() / 2);
 		g_pGame->AddConstructSprite(g_pHero);
-		g_pGame->AddDungeonSprite(g_pHero);
 	}
 }
 
@@ -785,101 +784,116 @@ void HandleKeys(WPARAM wParam)
 
 bool SpriteCollision(Sprite* pSpriteHitter, Sprite* pSpriteHittee)
 {
-  	// See if an enemy and hero have collided
-  	Bitmap* pHitter = pSpriteHitter->GetBitmap();
-  	Bitmap* pHittee = pSpriteHittee->GetBitmap();
+	Bitmap* pHitter = pSpriteHitter->GetBitmap();
+	Bitmap* pHittee = pSpriteHittee->GetBitmap();
 
-	/**********************The Following is an example of what happens here
+	//Check to see if hero collides with weapons in construct
+	if (isConstruct)
+	{
+		//Each collison with a weapon will set that weapon for the hero and then add the pointer of the sprite to
+		//the dungeon's sprite vector for update.  Then swap the level.
+		if (pHitter == g_pHero->GetBitmap() && pHittee == g_pConstructBoomerangBitmap)
+		{
+			g_pHero->SetWeapon(WT_BOOMERANG);
+			g_pGame->AddDungeonSprite(g_pHero);
+			isDungeon = true;
+			isConstruct = false;
+		}
+		if (pHitter == g_pHero->GetBitmap() && pHittee == g_pConstructBowBitmap)
+		{
+			g_pHero->SetWeapon(WT_BOW);
+			g_pGame->AddDungeonSprite(g_pHero);
+			isDungeon = true;
+			isConstruct = false;
+		}
+		if (pHitter == g_pHero->GetBitmap() && pHittee == g_pConstructSwordBitmap)
+		{
+			g_pHero->SetWeapon(WT_SWORD);
+			g_pGame->AddDungeonSprite(g_pHero);
+			isDungeon = true;
+			isConstruct = false;
+		}
+	}
 
-  	if ((pHitter == g_pMissileBitmap && (pHittee == g_pBlobboBitmap || pHittee == g_pJellyBitmap || pHittee == g_pTimmyBitmap)) || 
-	  			(pHittee == g_pMissileBitmap && (pHitter == g_pBlobboBitmap || pHitter == g_pJellyBitmap || pHitter == g_pTimmyBitmap)))
-  	{
-    	// Play the small explosion sound
-    	PlaySound((LPCSTR)IDW_LGEXPLODE, g_hInstance, SND_ASYNC | SND_RESOURCE);
+	if (isDungeon)
+	{
+		//Check to see if an enemy has ran into the hero
+		if (pHitter == g_pEnemyTenticalBitmap && pHittee == g_pHero->GetBitmap())
+		{
+			//Bounce the enemy off of hero
+			pSpriteHitter->SetVelocity(-pSpriteHitter->GetVelocity().x, -pSpriteHitter->GetVelocity().y);
+			//if hero's health is more than 1 subtract 1 but if hero's health is 1 then kill the sprite.
+			g_pHero->GetHealth() > 1 ? g_pHero->SetHealth(g_pHero->GetHealth() - 1) : pSpriteHittee->Kill(), g_bGameOver = true;
+		}
+		//Check to see if the boomerang has hit an enemy
+		if (pHitter == g_pBoomerangBitmap && pHittee == g_pEnemyTenticalBitmap)
+		{
+			pSpriteHitter->Kill();
+			//If enemey's health is more than 1 subtract 1 from it but kill it if it is at 1
+			if (pSpriteHittee->GetHealth() > 1)
+			{
+				pSpriteHitee->SetHealth(pSpriteHitee->GetHealth() - 1);
+			}
+			else
+			{
+				pSpriteHitee->Kill();
+				//If you kill the enemy play explosion
+				Sprite* pSprite = new Sprite(g_pEnemyExplosionBitmap, g_rcEnemeyBounds);
+				pSprite->SetNumFrames(9, true);
+				pSprite->SetPosition(pSpriteHittee->GetPosition());
+				g_pGame->AddDungeonSprite(pSprite);
+			}
+		}
+		//Check to see if an arrow has hit an enemy
+		if ((pHitter == g_pArrowLeftBitmap || pHitter == g_pArrowRightBitmap || pHitter == g_pArrowDownBitmap || pHitter == g_pArrowUpBitmap) && pHittee == g_pEnemyTenticalBitmap)
+		{
+			//Kill the arrow so it doesn't continue on through the enemy
+			pSpriteHitter->Kill();
 
-	    // Kill both sprites
-	    pSpriteHitter->Kill();
-	    pSpriteHittee->Kill();
-
-    	// Create a large explosion sprite at the alien's position
-    	RECT rcBounds = { 0, 0, 600, 450 };
-    	RECT rcPos;
-    	if (pHitter == g_pMissileBitmap)
-      		rcPos = pSpriteHittee->GetPosition();
-    	else
-      		rcPos = pSpriteHitter->GetPosition();
-    
-		Sprite* pSprite = new Sprite(g_pLgExplosionBitmap, rcBounds);
-    	pSprite->SetNumFrames(8, TRUE);
-    	pSprite->SetPosition(rcPos.left, rcPos.top);
-    	g_pGame->AddSprite(pSprite);
- 	}
-
-	// See if an alien missile has collided with the car
-  	if ((pHitter == g_pCarBitmap && (pHittee == g_pBMissileBitmap || pHittee == g_pJMissileBitmap || pHittee == g_pTMissileBitmap)) ||
-    		(pHittee == g_pCarBitmap && (pHitter == g_pBMissileBitmap || pHitter == g_pJMissileBitmap || pHitter == g_pTMissileBitmap)))
-  	{
-    	// Play the large explosion sound
-    	PlaySound((LPCSTR)IDW_LGEXPLODE, g_hInstance, SND_ASYNC | SND_RESOURCE);
-
-    	// Kill the missile sprite
-    	if (pHitter == g_pCarBitmap)
-      		pSpriteHittee->Kill();
-    	else
-      		pSpriteHitter->Kill();
-
-    	// Create a large explosion sprite at the car's position
-    	RECT rcBounds = { 0, 0, 600, 480 };
-    	RECT rcPos;
-    	if (pHitter == g_pCarBitmap)
-      		rcPos = pSpriteHitter->GetPosition();
-    	else
-      		rcPos = pSpriteHittee->GetPosition();
-    	
-		Sprite* pSprite = new Sprite(g_pLgExplosionBitmap, rcBounds);
-    	pSprite->SetNumFrames(8, TRUE);
-    	pSprite->SetPosition(rcPos.left, rcPos.top);
-    	g_pGame->AddSprite(pSprite);
-
-    	// Move the car back to the start
-    	g_pCarSprite->SetPosition(300, 405);
-
-    	// See if the game is over
-    	if (--g_iNumLives == 0)
-    	{
-      		// Play the game over sound
-      		PlaySound((LPCSTR)IDW_GAMEOVER, g_hInstance, SND_ASYNC | SND_RESOURCE);
-      		g_bGameOver = TRUE;
-      		g_iGameOverDelay = 150;
-
-    	}
-  	}
-
-	******************End of Example******************/
+			//If enemey's health is more than 1 subtract 1 from it but kill it if it is at 1
+			if (pSpriteHittee->GetHealth() > 1)
+			{
+				pSpriteHitee->SetHealth(pSpriteHitee->GetHealth() - 1);
+			}
+			else
+			{
+				pSpriteHitee->Kill();
+				//If you kill the enemy play explosion
+				Sprite* pSprite = new Sprite(g_pEnemyExplosionBitmap, g_rcEnemeyBounds);
+				pSprite->SetNumFrames(9, true);
+				pSprite->SetPosition(pSpriteHittee->GetPosition());
+				g_pGame->AddDungeonSprite(pSprite);
+			}
+		}
+		//Check to see if the sword hits the enemy
+		if ((pHitter == g_pSwordSwingLeftBitmap || pHitter == g_pSwordSwingRightBitmap || pHitter == g_pSwordSwingDownBitmap || pHitter == g_pSwordSwingUpBitmap) && pHittee == g_pEnemyTenticalBitmap)
+		{
+			//If enemey's health is more than 1 subtract 1 from it but kill it if it is at 1
+			if (pSpriteHittee->GetHealth() > 1)
+			{
+				pSpriteHitee->SetHealth(pSpriteHitee->GetHealth() - 1);
+			}
+			else
+			{
+				pSpriteHitee->Kill();
+				//If you kill the enemy play explosion
+				Sprite* pSprite = new Sprite(g_pEnemyExplosionBitmap, g_rcEnemeyBounds);
+				pSprite->SetNumFrames(9, true);
+				pSprite->SetPosition(pSpriteHittee->GetPosition());
+				g_pGame->AddDungeonSprite(pSprite);
+			}
+		}
+	}
 
   	return false;
 }
 
 void SpriteDying(Sprite* pSpriteDying)
 {
-	/******************The Following is an example of what would happen here
-
-  	// See if an alien missile sprite is dying
-  	if (pSpriteDying->GetBitmap() == g_pBMissileBitmap || pSpriteDying->GetBitmap() == g_pJMissileBitmap || pSpriteDying->GetBitmap() == g_pTMissileBitmap)
-  	{
-    	// Play the small explosion sound
-      	PlaySound((LPCSTR)IDW_SMEXPLODE, g_hInstance, SND_ASYNC | SND_RESOURCE | SND_NOSTOP);
-
-    	// Create a small explosion sprite at the missile's position
-    	RECT rcBounds = { 0, 0, 600, 450 };
-    	RECT rcPos = pSpriteDying->GetPosition();
-    	Sprite* pSprite = new Sprite(g_pSmExplosionBitmap, rcBounds);
-    	pSprite->SetNumFrames(8, TRUE);
-    	pSprite->SetPosition(rcPos.left, rcPos.top);
-    	g_pGame->AddSprite(pSprite);
-  	}
-
-	*******************End of Example*****************************************/
+	Sprite* pSprite = new Sprite(g_pEnemyExplosionBitmap, g_rcEnemeyBounds);
+	pSprite->SetNumFrames(9, true);
+	pSprite->SetPosition(pSpriteDying->GetPosition());
+	g_pGame->AddDungeonSprite(pSprite);
 }
 
 //-----------------------------------------------------------------
